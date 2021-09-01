602bd739b80add9e8d1198ca1f1c60c995cd93ef
Diana Deluvian <diana.deluvian@gmail.com>
2021-09-01 22:46:49 +0200

    added state, task add and delete functionality

--- to-do-react/src/components/Todo.js
+++ to-do-react/src/components/Todo.js
@@ -1,22 +1,32 @@
 import React from 'react';
 
 export default function Todo(props) {
-    console.log(props);
-    console.log(props.completed);
+  function deleteTask(id) {
+    console.log(id)
+  }
     return (
         <li className="todo stack-small">
           <div className="c-cb">
-            <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+            <input 
+              id={props.id} 
+              type="checkbox" 
+              defaultChecked={props.completed}
+              onChange={() => props.toggleTaskCompleted(props.id)}
+            />
             <label className="todo-label" htmlFor="todo-0">
               {props.name}
             </label>
           </div>
           <div className="btn-group">
             <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
+              Edit <span className="visually-hidden">{props.name}</span>
             </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
+            <button 
+            type="button" 
+            className="btn btn__danger"
+            onClick={() => props.deleteTask(props.id)}
+            >
+              Delete <span className="visually-hidden">{props.name}</span>
             </button>
           </div>
         </li>
